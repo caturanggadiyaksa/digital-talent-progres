a2f0c345647c70015584396aacaf35a73a5a0648
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2022-08-03 23:38:46 +0700

    initial commit

---
+++ implementasi-ke-coding/app.js
@@ -0,0 +1,21 @@
+
+//deklarasi untuk variable phi 3.14
+const phi = 3.14;
+
+
+
+// deklarasi varibale input jari jari  dan menerima inputan dari user untuk memasukan jari jari
+const inputJarijari = prompt("Masukan jari jari:"); // jari jari akan disimpan di variable input jari jari
+
+
+//deklarasi variable rumus lingkaran 
+const rumusLingkaran = phi * inputJarijari * inputJarijari;
+
+
+//menampilkan rumus lingkaran
+alert(`Luas Lingkaran${rumusLingkaran}`);
+
+
+
+
+
