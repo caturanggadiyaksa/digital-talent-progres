15b44ff877e5ed76d9e4d2b8f7bea8ecbd9ef2d2
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2022-08-04 08:01:18 +0700

    intial commit

--- implementasi-ke-coding/lingkaran.js
+++ implementasi-ke-coding/lingkaran.js
@@ -1,22 +1,27 @@
 
 
 /*LUAS LINGKARAN */
-
-//deklarasi untuk variable phi 3.14
-const phi = 3.14;
+function luasLingkaran(){
 
 
-
-// deklarasi varibale input jari jari  dan menerima inputan dari user untuk memasukan jari jari
-const inputJarijari = prompt("Masukan jari jari:"); // jari jari akan disimpan di variable input jari jari
-
-
-//deklarasi variable rumus lingkaran 
-const rumusLingkaran = phi * inputJarijari * inputJarijari;
+    //deklarasi untuk variable phi 3.14
+    const phi = 3.14;
 
 
-//menampilkan rumus lingkaran
-alert(`Luas Lingkaran${rumusLingkaran}`);
+
+    // deklarasi varibale input jari jari  dan menerima inputan dari user untuk memasukan jari jari
+    const inputJarijari = prompt("Masukan jari jari:"); // jari jari akan disimpan di variable input jari jari
+
+
+    //deklarasi variable rumus lingkaran 
+    const rumusLingkaran = phi * inputJarijari * inputJarijari;
+
+
+    //menampilkan rumus lingkaran
+    alert(`Luas Lingkaran${rumusLingkaran}`);
+
+}
+
 
 
 
